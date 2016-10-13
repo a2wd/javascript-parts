97d2abef331cbfdbf6d1210b18147326257ecb2d
a2wd <github@a2wd.com>
2016-10-13 16:58:14 +0100

    Just started: chapter 5

--- Chapter 5/script.js
+++ Chapter 5/script.js
@@ -2,6 +2,7 @@ var notes = [];
 
 notes.push("<strong>Chapter 5, Inheritance</strong>");
 
+notes.push("In javascript, objects can inherit from objects. There are no classes (pre ECMA Script 6) ")
 
 
 //Write notes to DOM
